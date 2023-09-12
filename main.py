5e8f1ee9912b0eac83b82208c568603ccb0494f3
tastefulblatant <121983707+tastefulblatant@users.noreply.github.com>
2023-09-12 18:19:28 +0200

    .

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def game():
   print(f'         {Fore.LIGHTBLACK_EX} -----------------------------------------------------------{Fore.RESET}')
   time.sleep(1)
   print(f'                                                                                                    ')
-  print(f"         {Fore.LIGHTMAGENTA_EX}Vítejte v naší hře            DEV by Jakub Hošek & Matěj Mrkus{Fore.RESET}")  #hosku to uvítání nejak přepis, to "Vítejte v naší hře", dej tam neco jinyho je to dogshitXD
+  print(f"         {Fore.LIGHTMAGENTA_EX}Vítejte v naší hře            DEV by Jakub Hošek & Matěj Mrkus{Fore.RESET}") 
   print(f'                                                                                                    ')
   print(f'                                                                                                    ')
 
@@ -105,7 +105,6 @@ def game():
             file.write(points)
           points = int(points)
           end()
-            
   #kamen nuzky papir-------------------------------------------------    
       if casino_choice == "k":
           rock = '''
@@ -423,10 +422,6 @@ def game():
             points = int(points)
             end()
   #-------------------------------------------------     
-        
-      elif casino_choice == "kubamatej":
-    
-          print("Co tady děláš???")
 
       elif casino_choice == "rickroll":
         print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
